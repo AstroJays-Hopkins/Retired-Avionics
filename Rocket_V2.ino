8c70e947587b3331c0f08c14e6c230e286ce3c73
rranisz1 <36429469+rranisz1@users.noreply.github.com>
2018-04-08 14:05:49 -0400

    Add files via upload
    Updated version of code to include datalogging to SD card

--- Rocket_V2.ino
+++ Rocket_V2.ino
@@ -1,15 +1,18 @@
-#include <RH_RF95.h>
-#include <Adafruit_GPS.h>
+//#include <RH_RF95.h>
+//#include <Adafruit_GPS.h>
 #include <Wire.h>
 #include <SoftwareSerial.h>
+#include <SD.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BNO055.h>
 #include <utility/imumaths.h>
 #include "IntersemaBaro.h"
 
+const int CS = 4;
+
 float Flight_Log[9] = {0,0,0,0,0,0,0,0,0};
 //%%%%%%%%%%%%%%%%%%%%%%%% GPS SETUP %%%%%%%%%%%%%%%%%%%%%%%%//
-
+/*
 float latitude,longitude;
 
 //what's the name of the hardware serial port?
@@ -22,18 +25,19 @@ Adafruit_GPS GPS(&GPSSerial);
 //Set GPSECHO to 'false' to turn off echoing the GPS data to the Serial console
 //Set to 'true' if you want to debug and listen to the raw GPS sentences
 #define GPSECHO  true
-
+*/
 //%%%%%%%%%%%%%%%%%%%%%%%% ALTIMETER SETUP %%%%%%%%%%%%%%%%%%%%%%%%//
 
 Intersema::BaroPressure_MS5607B baro(true);
 
 // Altitude variables
-float avg_alt;
+int avg_alt;
 int alt0;
-float altitude;
-float old_Alt;
-float new_Alt;
-unsigned long t0;
+int altitude;
+int old_Alt;
+int new_Alt;
+unsigned long T0;
+unsigned long T1;
 unsigned long T;
 
 //create state machine for deployment stages
@@ -48,6 +52,7 @@ enum states{
 
 //set initial state
 states flight = None;
+states timer = None;
 
 
 
@@ -59,10 +64,9 @@ Adafruit_BNO055 bno = Adafruit_BNO055(55);
 int ang[3] = {0,0,0};
 
 
-
 //%%%%%%%%%%%%%%%%%%%%%%%% ACCELEROMETER SETUP %%%%%%%%%%%%%%%%%%%%%%%%//
 
-/*float xRawMin = 0;
+float xRawMin = 0;
 float xRawMax = 512;
  
 float yRawMin = 0;
@@ -70,34 +74,40 @@ float yRawMax = 512;
  
 float zRawMin = 0;
 float zRawMax = 512;
-*/
+
 int acc[3] = {0,0,0}; //accelerometer readout array; x, y, z axes respectively
 
 //%%%%%%%%%%%%%%%%%%%%%%%% RECOVERY PIN SETUP %%%%%%%%%%%%%%%%%%%%%%%%//
 
-int REC[2] = {3,4}; //recovery pins
-
+int R = 5;
 //%%%%%%%%%%%%%%%%%%%%%%%% PAYLOAD PIN SETUP %%%%%%%%%%%%%%%%%%%%%%%%//
 
-int PL = 5;
+int PL = 6;
 
 //%%%%%%%%%%%%%%%%%%%%%%%% RADIO SETUP %%%%%%%%%%%%%%%%%%%%%%%%//
 int loop_counter = 0;
-RH_RF95 rf95;
-#define RF95_FREQ 915.0
+//RH_RF95 rf95;
+//#define RF95_FREQ 915.0
 
 void setup() {
-Serial.begin(9600);
+//begin communication
+Serial.begin(9600); //set to 115200 when using GPS
+//GPS.begin(9600);
+
+if (!SD.begin(CS)) {
+    Serial.println("Card failed, or not present");
+    return;
+}
 //initialize pins
 pinMode(A1, INPUT);
 pinMode(A2, INPUT);
 pinMode(A3, INPUT);
-pinMode(REC, OUTPUT);
+pinMode(R, OUTPUT);
 pinMode(PL, OUTPUT);
-digitalWrite(REC, LOW);
+digitalWrite(R, LOW);
 digitalWrite(PL, LOW);
 
-
+Serial.println("Booted up");
 //initialize sensors
 baro.init();
 bno.begin();
@@ -118,13 +128,14 @@ for (int i=0; i<num_points; i++)
       delay(10);
     }
 alt0 /= num_points;            //normalize to the number of samples collected
-//set initial state
+
+//set initial states
 flight = Setup;
-//states timer = Setup;
+timer = None;
 }
 
 
-/*//create a loop to calculate average acceleration values
+//create a loop to calculate average acceleration values
 float ReadAxis(int axisPin)
 {
   long reading = 0;
@@ -136,14 +147,45 @@ float ReadAxis(int axisPin)
   return reading/10;
 }
 
-*/
+void AutoCalibrate(int xRaw, int yRaw, int zRaw)
+{
+  //Serial.println("Calibrate");
+  if (xRaw < xRawMin)
+  {
+    xRawMin = xRaw;
+  }
+  if (xRaw > xRawMax)
+  {
+    xRawMax = xRaw;
+  }
+  
+  if (yRaw < yRawMin)
+  {
+    yRawMin = yRaw;
+  }
+  if (yRaw > yRawMax)
+  {
+    yRawMax = yRaw;
+  }
+ 
+  if (zRaw < zRawMin)
+  {
+    zRawMin = zRaw;
+  }
+  if (zRaw > zRawMax)
+  {
+    zRawMax = zRaw;
+  }
+}
+
+
 void loop() {
 
   ////////// Altimeter //////////
   altitude = baro.getHeightCentiMeters()/30.48 - alt0;
   avg_alt += (altitude - avg_alt)/5;
   
- /*////////// Accelerometer //////////
+////////// Accelerometer //////////
   float xRaw = ReadAxis(A1); //take accelerometer voltages in X,Y,Z directions
   float yRaw = ReadAxis(A2);
   float zRaw = ReadAxis(A3);
@@ -158,7 +200,8 @@ void loop() {
 
   acc[1] = AX; //write acceleration vector values to array
   acc[2] = AY;
-  acc[3] = AZ;*/
+  acc[3] = AZ;
+
   
   ////////// IMU //////////
   sensors_event_t event;
@@ -167,26 +210,34 @@ void loop() {
   ang[2] = {(int)event.orientation.y};
   ang[3] = {(int)event.orientation.z};
   delay(BNO055_SAMPLERATE_DELAY_MS);
+  
+  
   ////////// Recovery and Payload Activation //////////
+  
+  //ALTMETER BASED DEPLOYMENT LOOP//
+  
   switch(flight) {
     case Setup:
-      Serial.println("SETUP");
-        if(avg_alt > 5){
+      //Serial.println("SETUP");
+        if(avg_alt > 10){
           flight = Launchpad;
         }
       break;
       
       
     case Launchpad:
-      Serial.println("LAUNCHPAD");
+      //Serial.println("LAUNCHPAD");
       old_Alt = avg_alt;
+      AutoCalibrate(xRaw,yRaw,zRaw);
       if(old_Alt > 100){
+        T0 = millis();
         flight = Thrust;
+        timer = Thrust;
       }
       break;
     
     case Thrust:
-      Serial.println("THRUST");
+     // Serial.println("THRUST");
       new_Alt = avg_alt;
       T = millis();
       if ((new_Alt - old_Alt)/(millis() - T) < 0){
@@ -197,29 +248,50 @@ void loop() {
       break;
 
     case Descent:
-      Serial.println("DESCENT");
-      if (avg_alt < 300){
-        digitalWrite(REC, HIGH);
-        Serial.println("RECOVER");
+      //Serial.println("DESCENT");
+      if (avg_alt < 400){
+        digitalWrite(R, HIGH);
+        //Serial.println("RECOVER");
       }
   }
+
+
+  //TIMER BASED DEPLOYMENT LOOP//
+
+  switch(timer){
+    case Thrust:
+      T1 = millis();
+      if(T1 > T0 + 20000){
+        digitalWrite(PL, HIGH);
+        T0 = millis();
+        timer = Descent;
+      }
+      break;
+      
+    case Descent:
+      T1 = millis();
+      if (T1 > T0 + 60000){
+        digitalWrite(R, HIGH);
+      }
+  }
+
   
   ////////// GPS //////////
-  latitude = GPS.latitude;
-  longitude = GPS.latitude;
+// latitude = GPS.latitudeDegrees;
+// longitude = GPS.latitudeDegrees;
 
 
   ////////// link data to arrays //////////
 
-  Flight_Log[1] = {altitude};
-  Flight_Log[2] = {0};
-  Flight_Log[3] = {0};
-  Flight_Log[4] = {0};
+  Flight_Log[1] = {avg_alt};
+  Flight_Log[2] = {acc[1]};
+  Flight_Log[3] = {acc[2]};
+  Flight_Log[4] = {acc[3]};
   Flight_Log[5] = {ang[1]};
   Flight_Log[6] = {ang[2]};
   Flight_Log[7] = {ang[3]};
-  Flight_Log[8] = {latitude};
-  Flight_Log[9] = {longitude};
+//  Flight_Log[8] = {latitude};
+//  Flight_Log[9] = {longitude};
 
 
  if (loop_counter % 20 == 0) {
@@ -234,8 +306,29 @@ void loop() {
   Serial.println(Flight_Log[7]);
  }
  loop_counter++;
- /* if (!rf95.available()){
-          Serial.println("NO RADIO");
+
+ String data = "";
+
+ for (int i = 0; i < 7; i++){
+  data += String(Flight_Log[i]);
+  if (i<6){
+    data += ",";
+  }
+ }
+
+ File dataFile = SD.open("Flight_Log.txt", FILE_WRITE);
+
+ if (dataFile){
+  dataFile.println(data);
+  dataFile.close();
+  Serial.println(data);
+ }
+ else{
+  Serial.println("error opening Flight_Log.txt");
+ }
+ /*
+ if (!rf95.available()){
+         Serial.println("NO RADIO");
   }
   else{
     
@@ -244,17 +337,19 @@ void loop() {
     uint8_t len = sizeof(buf);
     if (rf95.recv(buf, &len))
     {
-//      RH_RF95::printBuffer("request: ", buf, len);
-      Serial.print("got request: ");
-      Serial.println((char*)buf);
-      Serial.print("RSSI: ");
-//      Serial.println(rf95.lastRssi(), DEC);
+      RH_RF95::printBuffer("request: ", buf, len);
+      //Serial.print("got request: ");
+      //Serial.println((char*)buf);
+      //Serial.print("RSSI: ");
+      //Serial.println(rf95.lastRssi(), DEC);
       
       // Send a reply
       uint8_t data[] = {Flight_Log};
       rf95.send(data,sizeof(data));
       rf95.waitPacketSent();
     }
-  }(*/
+  }
+  */
  }
+
 
