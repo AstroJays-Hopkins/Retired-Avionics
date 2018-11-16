49d90ed12947afb4f641dc4efe706aa5fbf673e2
avnoks <ghuwrov@the-huangs.net>
2018-11-16 18:06:28 -0500

    Incorporate music into MEGA code so rocket can play tunes.

---
+++ MEGA/music-scores.h
@@ -0,0 +1,32 @@
+/* Musical 'scores' for tunes played by rocket */
+
+const int ASZ_BEGINNING[] =
+  {MUSIC_TEMPO, 70,
+   MUSIC_TIME_SIG_LOWER, 4,
+   
+   NOTE_C4, -2, NOTE_G4, -2, NOTE_C5, -3,
+   NOTE_REST, 8,
+   NOTE_DS5, 8, NOTE_D5, -4,
+   NOTE_REST, -2,
+
+   /* NOTE_C4, -2, NOTE_G4, -2, NOTE_C5, -3, */
+   /* NOTE_REST, 8, */
+   /* NOTE_DS5, 8, NOTE_E5, -4, */
+   /* NOTE_REST, -2, */
+   
+   /* NOTE_C4, -2, NOTE_G4, -2, NOTE_C5, -3, */
+   /* NOTE_REST, 8, */
+   /* NOTE_E5, 8, NOTE_F5, -4, */
+   
+   /* NOTE_A5, 8, NOTE_B5, 8, */
+   /* NOTE_C6, -2, NOTE_D6, -1, */
+   /* NOTE_E5, 8, NOTE_F5, 8,  */
+   /* NOTE_G5, 4, NOTE_E5, 4, NOTE_C5, 4, NOTE_G4, 4, */
+   
+   /* NOTE_E5, 8, NOTE_F5, 8, */
+   /* NOTE_G5, 4, NOTE_E5, 4, NOTE_C5, 4, NOTE_G4, 4, */
+   
+   /* NOTE_E5, 8, NOTE_F5, 8, */
+   /* NOTE_G5, -3, NOTE_A5, -3, NOTE_B5, -3, NOTE_C6, -3, */
+   /* MUSIC_STOP */
+};
