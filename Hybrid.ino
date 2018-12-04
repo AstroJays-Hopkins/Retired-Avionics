8f46bf626d26c21bf19bb676e604731b851f1e57
rranisz1 <36429469+rranisz1@users.noreply.github.com>
2018-12-04 16:17:19 -0500

    Update Hybrid.ino
    Updated thermocouple sensor/data array structures, redefined value for ignition command, nested ignition command in conditional statement that tests for arming condition so ignition cannot occur during fueling

--- Hybrid.ino
+++ Hybrid.ino
@@ -1,24 +1,24 @@
 #include <RF24.h>
-
 #include <Wire.h>
 #include <Servo.h>
 #include "Adafruit_MAX31855.h"
 
-//create servo objects
+//create servo objects for ball valve control
 Servo Burn;
 
 //create an array for the conditions of the servos
 boolean Valves [2];
 
-//define the pin to regulate the emergency burp valve
+//define the pin to toggle the emergency burp valve
 int Emergency = 3;
 
-float PT [3] = {0,0,0};
+//define the pin used for the ignition relay
+int LIT = 4;
 
 //create arrays for temperature and pressure values across 
 //the run tank and combustion chamber
-float TCrun [2]= {0,0};
-float TCburn [2] = {0,0};
+float TC [4]= {0,0,0,0};
+float PT [3] = {0,0,0};
 
 //declare SPI pins for thermocouple breakout
 int MAXDO = 50;
@@ -32,7 +32,7 @@ Adafruit_MAX31855 thermocouple2(MAXCLK, TCCS[1], MAXDO);
 Adafruit_MAX31855 thermocouple3(MAXCLK, TCCS[2], MAXDO);
 Adafruit_MAX31855 thermocouple4(MAXCLK, TCCS[3], MAXDO);
 
-Adafruit_MAX31855 TC [4] = {thermocouple1,thermocouple2,thermocouple3,thermocouple4};
+Adafruit_MAX31855 Thermocouples [4] = {thermocouple1,thermocouple2,thermocouple3,thermocouple4};
 
 
 
@@ -41,7 +41,9 @@ byte Pins[3] = {A0,A1,A2};
 
 //define condition for ignition to open ball valve
 int ARMED = 4; //put switch at pin 4 
-int FIRE;
+
+//define a specific value as our launch command code
+int LIVE = 420;
 
 //radio communication setup
 bool radioNumber = 0;
@@ -60,6 +62,9 @@ void setup() {
   for(int i = 0; i < 2; i++){
   Valves[i] = false;
   }
+  
+  //ensure ignition pin is turned off
+  digitalWrite(LIT,LOW);
   
   //attach valves to PWM pins
   Burn.attach(5);
@@ -103,15 +108,14 @@ void setup() {
 
 
 void loop() {
-  //determine if launch signal has been sent
-  if (digitalRead(ARMED) == HIGH){ //MOVE CONDITION TO END OF LOOP
 
-  //do not vent in flight, therefore do not transmit data after launch command has been issued
-  while(FIRE = 0){
+  //do not vent in flight, therefore do not transmit/receive data after launch command has been issued
+  while(FIRE = 0){  
     for(int j = 0; j < 2; j++){
-      MAXCS = TCCS[j];
-      TCrun[j] = TC[j].readCelsius();
-      TCburn[j+2] = TC[j].readCelsius();
+      MAXCS = TCCS[j];  //define CS pin for each thermocouple in the run tank
+      TC[j] = Thermocouples[j].readCelsius(); //take run tank temperatures
+      MAXCS = TCCS[j+2]; //define CS pin for each thermocouple in the combustion chamber
+      TC[j+2] = Thermocouples[j+2].readCelsius(); //take combustion chamber temperatures
     }
     
     for(int k = 0; k < 3; k++){
@@ -119,17 +123,21 @@ void loop() {
       PT[k] = map(PT[k],0,1023,0,10000);
     }
   
-    for (int l = 0; l < 2; l++){
-      if(TCrun[l] >= 309.5){
-          Valves[0] == true;
+    for (int l = 0; l < 2; l++){ //test for critical temperature: open solenoid if above threshold, close if below
+      if(TC[l] >= 309.5){
+          Valves[0] = true;
           Serial.println("TEMPERATURE CRITICAL");
+      }else{
+        Valves[0] = false;
       }
     }
 
-    for (int m = 0; m < 3; m++){
+    for (int m = 0; m < 3; m++){ //test for critical pressure: open solenoid if above threshold, close if below
       if(PT[m] >= 7240){
         Valves[0] == true;
         Serial.println("PRESSURE CRITICAL");
+      }else{
+        Valves[1] = false;
       }
     }
     
@@ -149,15 +157,16 @@ void loop() {
     Serial.println(F("Now sending"));
     for (int a = 0; a < 4; a++){
 
-      unsigned long temp = TCrun[a];                             // Take the temperature, and send it.  This will block until complete
+      unsigned long temp = TC[a];                             // Take the temperature, and send it.  This will block until complete
       if (!radio.write( &temp, sizeof(unsigned long) )){
         Serial.println(F("failed"));
         }else{
         radio.write(&temp, sizeof(unsigned long));
       }
-      }
+    }
+    
     for (int b = 0; b < 3; b++){
-      unsigned long Pressure = PT[b];                             // Take the temperature, and send it.  This will block until complete
+      unsigned long Pressure = PT[b];                             // Take the pressure, and send it.  This will block until complete
       if (!radio.write( &Pressure, sizeof(unsigned long) )){
         Serial.println(F("failed"));
       }else{
@@ -165,20 +174,23 @@ void loop() {
       }
     }
     radio.startListening();                                    // Now, continue listening
-      
+    
     int FIRE;               // Set up a timeout period, get the current microseconds
     
-    
-    if( radio.available()){
+    //determine if launch signal has been sent
+      if (digitalRead(ARMED) == HIGH){ 
+        if( radio.available()){
                                                                     // Variable for the received command
-      while (radio.available()) {                                   // While there is data ready
-        radio.read( &FIRE, sizeof(int) );             // Get the payload
+          while (radio.available()) {                                   // While there is data ready
+            radio.read( &FIRE, sizeof(int) );             // Get the fire command message
+          }
+        }
+        
+      if (FIRE == LIVE){    //if fire command matches the value we determine
+        Valves[1] = true;   //open the ball valve 
+        digitalWrite(LIT,HIGH);   //ignite the solid fuel grain
       }
-    }
-    if (FIRE = 1){
-      Valves[1] = true; //ADD IGNITION PIN FOR RELAY
-    }
-  }  
+    }  
   }
   radio.stopListening();
 } 
