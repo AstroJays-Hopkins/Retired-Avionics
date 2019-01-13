82091df36a7c9b5dd589b85c9bdf6a859f837148
t0rp3do <43256272+t0rp3do@users.noreply.github.com>
2019-01-13 13:02:16 -0500

    State-unique songs
    Added armed, thrust, descent, and recovery sounds

--- MEGA/music-scores.h
+++ MEGA/music-scores.h
@@ -9,25 +9,41 @@ const int ASZ_BEGINNING[] =
    NOTE_DS5, 8, NOTE_D5, -4,
    NOTE_REST, -2,
    MUSIC_STOP
-
-   /* NOTE_C4, -2, NOTE_G4, -2, NOTE_C5, -3, */
-   /* NOTE_REST, 8, */
-   /* NOTE_DS5, 8, NOTE_E5, -4, */
-   /* NOTE_REST, -2, */
-   
-   /* NOTE_C4, -2, NOTE_G4, -2, NOTE_C5, -3, */
-   /* NOTE_REST, 8, */
-   /* NOTE_E5, 8, NOTE_F5, -4, */
-   
-   /* NOTE_A5, 8, NOTE_B5, 8, */
-   /* NOTE_C6, -2, NOTE_D6, -1, */
-   /* NOTE_E5, 8, NOTE_F5, 8,  */
-   /* NOTE_G5, 4, NOTE_E5, 4, NOTE_C5, 4, NOTE_G4, 4, */
-   
-   /* NOTE_E5, 8, NOTE_F5, 8, */
-   /* NOTE_G5, 4, NOTE_E5, 4, NOTE_C5, 4, NOTE_G4, 4, */
-   
-   /* NOTE_E5, 8, NOTE_F5, 8, */
-   /* NOTE_G5, -3, NOTE_A5, -3, NOTE_B5, -3, NOTE_C6, -3, */
-   /* MUSIC_STOP */
 };
+
+const int ARMED_SIGNAL[] =
+  {MUSIC_TEMPO, 160,
+    MUSIC_TIME_SIG_LOWER, 4,
+
+    NOTE_A5, -1, NOTE_A5, -1, NOTE_A5, -1, NOTE_A5, -1,
+    NOTE_REST, -8,
+    MUSIC_REPEAT
+  };
+
+const int THRUST_SIGNAL[] =
+  {MUSIC_TEMPO, 320,
+    MUSIC_TIME_SIG_LOWER, 4,
+
+    NOTE_A5, -1, NOTE_A5, -1, NOTE_A5, -1,
+    NOTE_REST, -1,
+    MUSIC_REPEAT
+  };
+
+const int DESCENT_SIGNAL[] =
+  {MUSIC_TEMPO, 200,
+    MUSIC_TIME_SIG_LOWER, 4,
+
+    NOTE_A5, -1, NOTE_A5, -1,
+    NOTE_REST, -2,
+    MUSIC_REPEAT
+  };
+
+const int RECOVERY_SIGNAL[] =
+  {MUSIC_TEMPO, 140,
+    MUSIC_TIME_SIG_LOWER, 4,
+
+    NOTE_A5, -1, NOTE_A5, -1, NOTE_A5, -1,
+    NOTE_REST, -1,
+    MUSIC_REPEAT
+  };
+  
