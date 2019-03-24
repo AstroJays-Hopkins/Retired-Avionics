49660f81040918ae67acb6b2da7d1fe846234c2b
rranisz1 <36429469+rranisz1@users.noreply.github.com>
2019-03-24 18:05:56 -0400

    defined setup/transmission functions

--- propulsion/darts/Server.py
+++ propulsion/darts/Server.py
@@ -1,13 +1,15 @@
-import socket
 
-s = socket.socket()
-host = '' #ip of raspberry pi
-port = 12345
-s.bind((host, port))
+def serverSetup():
+  import socket
+  s = socket.socket()
+  host = '' #ip of raspberry pi
+  port = 12345
+  s.bind((host, port))
 
-s.listen(5)
-while True:
-  c, addr = s.accept()
-  print ('Got connection from',addr)
-  c.send('Thank you for connecting')
-  c.close()
+def serverSend():
+  s.listen(5)
+  while True:
+    c, addr = s.accept()
+    print ('Got connection from',addr)
+    c.send('Thank you for connecting')
+    c.close()
