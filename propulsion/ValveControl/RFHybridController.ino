e1174053991ccb2e6a8d4779a2504110e3298f8a
rranisz1 <36429469+rranisz1@users.noreply.github.com>
2019-04-25 21:01:36 -0400

    Add files via upload

---
+++ propulsion/ValveControl/RFHybridController.ino
@@ -0,0 +1,84 @@
+#include <SPI.h>
+#include <LoRa.h>
+
+//declare pins for fueling, venting, and disconnect/reset switches
+const int fuelSwitch = 4;
+const int ventSwitch = 5;
+const int dcSwitch = 6;
+const int resetSwitch = 7;
+
+//VARIABLE FOR IGNITION BUTTON
+const int ignitionSwitch = 8;
+
+//set variables for the condition of each switch
+//this corresponds to the desired state of the analogous solenoid
+String IGNITION;
+String Command1;
+String Command2;
+String Command3;
+
+//variable to store list of desired states
+String Command;
+
+void setup() {
+  //begin RF communiation
+  LoRa.begin(915E6);
+  Serial.begin(9600);
+  //set all switch pins to receive voltage
+  pinMode(ventSwitch, INPUT);
+  pinMode(dcSwitch, INPUT);
+  pinMode(resetSwitch, INPUT);
+  pinMode(fuelSwitch, INPUT);
+}
+
+void ventCom(){
+  if (digitalRead(ventSwitch) == HIGH){
+    Command1 = "V"; //indicate venting command was sent
+  }
+}
+
+void dcCom(){
+  if (digitalRead(dcSwitch) == HIGH){
+    Command2 = "D"; //indicate DC command was sent
+  }
+}
+
+void resetCom(){
+  if (digitalRead(resetSwitch) == HIGH){
+    Command2 = "R"; //indicate reset variable was sent
+  }
+}
+
+void fuelCom() {
+  if (digitalRead(fuelSwitch) == LOW){
+    Command3 = "F"; //send ignition variable
+  }else{
+    Command3 = "C";
+  }
+}
+
+void ignitionCom() {
+  if (digitalRead(ignitionSwitch == HIGH){
+    IGNITION = "I"; //set ignition variable 
+  }
+}
+
+void getCom() {
+  ventCom();
+  dcCom();
+  resetCom();
+  fuelCom(); //check states, send commands to open/close valves to rocket
+  Command = IGNITION + Command1 + Command2 + Command3;
+}
+
+void loop() {
+  IGNITION = "X"; //set default state variables that correspond to valve default states
+  Command1 = "X";
+  Command2 = "X";
+  Command3 = "X";
+  getCom();
+  Serial.println(Command); //DEBUG: display what you sent as the command
+  LoRa.beginPacket(); //start transmission
+  LoRa.print(Command); //just gonna send it, asuhh dudes
+  LoRa.endPacket(); //end transmission
+}
---
+++ propulsion/ValveControl/RFValves.ino
@@ -0,0 +1,90 @@
+#include <LoRa.h>
+
+//declare pins for solenoid relays
+const int fuelRelay = 4;
+const int ventRelay = 5;
+const int dcRelay = 6;
+const int resetRelay = 7;
+
+//set variable for received RF command
+String rfInput;
+
+void setup() {
+  Serial.begin(9600);
+  
+  //begin RF communication
+  LoRa.begin(915E6);
+
+  //set relay pins to output voltage
+  pinMode(ventRelay,OUTPUT);
+  pinMode(dcRelay,OUTPUT);
+  pinMode(resetRelay,OUTPUT);
+
+  //set default relay states to LOW (default)
+  digitalWrite(ventRelay,LOW);
+  digitalWrite(dcRelay,LOW);
+  digitalWrite(resetRelay,LOW);
+}
+
+void vent() { //open venting solenoid
+  digitalWrite(ventRelay,HIGH);
+  Serial.println("VENTING");
+}
+
+void DC() { //disconnect fueling line
+  digitalWrite(dcRelay,HIGH);
+  Serial.println("DISCONNECTING FUELING LINE");
+}
+
+void RESET() { //reset fueling line linear actuator
+  digitalWrite(resetRelay,HIGH);
+  Serial.println("RESETTING FUELING LINE");
+}
+
+void FUEL() { //open fueling valve
+  digitalWrite(fuelRelay,HIGH);
+  Serial.println("FUELING VALVE OPEN");
+}
+
+void DONE() { //close fueling valve
+  digitalWrite(fuelRelay,LOW);
+  Serial.println("FUELING VALVE CLOSED");
+}
+
+void CLOSE() { //close all valves
+  digitalWrite(dcRelay,LOW);
+  digitalWrite(resetRelay,LOW);
+  digitalWrite(ventRelay,LOW);
+}
+
+void loop() {
+  int packetSize = LoRa.parsePacket();
+  if (packetSize){ //receive RF commands
+    while (LoRa.available()) {
+      rfInput += (char)LoRa.read(); //append string with each solenoid state condition
+    }  
+  }
+  Serial.println(rfInput); //DEBUG: display RF command
+
+  //Check states
+  if(rfInput[1] == "V"){
+    vent();
+  }
+  
+  if(rfInput[2] == "D"){
+    DC();
+  }else if(rfInput[2] == "R"){
+    RESET();
+  }
+
+  if(rfInput[3] == "F"){
+    FUEL();
+  }else if(rfInput[3] == "C"){
+    DONE();
+  }
+  if(rfInput[0] == "I"){
+    CLOSE();
+    Serial.println("FIRING");
+    
+  }
+}
