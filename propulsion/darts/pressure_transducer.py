5d97bfc9c1e17a422f497534d21eaff9512277c2
Mixih <mxhahn1@gmail.com>
2019-10-13 21:18:20 -0400

    darts/pressure_transducer: Use Reader Class
    Converted the scattered reading functions to a reader class to allow
    easier PressureTransducer object manipulation.

--- propulsion/darts/pressure_transducer.py
+++ propulsion/darts/pressure_transducer.py
@@ -3,19 +3,9 @@ import busio
 import adafruit_ads1x15.ads1115 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
 
-try:
-    # Create the I2C bus
-    i2c = busio.I2C(board.SCL, board.SDA)
-
-    # Create the ADC object using the I2C bus
-    Global_ADS = ADS.ADS1115(i2c)
-except Exception as e:
-    print(e)
-
-# Global_ADS.gain = 2
 
 class PressureTransducer:
-    def __init__(self, adc_channel):
+    def __init__(self, Global_ADS, adc_channel):
         self.chan = AnalogIn(Global_ADS, adc_channel)
         self.last_reading = -1
     # PT produces maximum of 4.5V at 5076.32 PSI
@@ -31,15 +21,49 @@ class PressureTransducer:
             return "E"
 
 
-def readPressureTransducers(PTarray):
-    data = []
-    # for sensor in PTArray:
-    for i in range(4):
+class PressureTransducerReader:
+    '''
+    Batch reader for more than one pressure transducer. You probably want to
+    use this.
+    '''
+    def __init__(self, adc_channels):
+        '''
+        Initialize i2c bus and create one PressureTransducer object per channel
+        specified in pt_channels
+
+        :param pt_channels: list of PT channels to use. Note that read will be
+                            returned in this order
+        '''
+        self.PTs = []
         try:
-            data.append(PTarray[i].readPressure())
+            # Create the I2C bus
+            self.i2c = busio.I2C(board.SCL, board.SDA)
+            # Create the ADC object using the I2C bus
+            self.ADS = ADS.ADS1115(self.i2c)
         except Exception as e:
-            data.append("E[PT]")
-            print(str(e))
-        # data.append(sensor.chan.voltage)
-        # data.append(sensor.chan.value)
-    return data 
+            print(e)
+        for channel in adc_channels:
+            try:
+                self.PTs.append(PressureTransducer(self.ADS, channel))
+            except Exception as e:
+                print(e)
+                # kludge to take care of case where specified channel has no PT
+                self.PTs.append(None)
+
+    def readPTs(self):
+        '''
+        Read PTs specified when this reader class was initialized.
+
+        :return: List of PT readings in order specified when this class was
+                 initialized
+        '''
+        data = []
+        for PT in self.PTs:
+            try:
+                data.append(PT.readPressure())
+            except Exception as e:
+                data.append("E[PT]")
+                print(str(e))
+            # data.append(sensor.chan.voltage)
+            # data.append(sensor.chan.value)
+        return data
