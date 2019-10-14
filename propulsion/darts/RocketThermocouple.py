dfb2a6aa22fee90a822624124def07f78a307318
Mixih <mxhahn1@gmail.com>
2019-10-13 20:54:48 -0400

    darts/RocketThermocouple: Adapt to new paradigm
    Move reading functions into a reading lcass to package data with
    associated methods. This should also solve the "erroring thermocouples
    always come last, no matter what port they're plugged into bug" by
    reserving a list of TC object in the reader class.

--- propulsion/darts/RocketThermocouple.py
+++ propulsion/darts/RocketThermocouple.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
-# Author: Courtney Schmitt 
+# Author: Courtney Schmitt
 # Date Created: 03/17/2019
 #
-# Last Modified: 03/17/2019 
+# Last Modified: 03/17/2019
 #
 # Creates object RocketThermocouple of Adafruit MAX31855 type extends readTempC
 # Iterates through TC objects and takes each measurement, stores to list and returns# the list of temperature readings.
@@ -38,13 +38,61 @@ class Thermocouple(MAX31855.MAX31855):
             return self.last_reading
         except Exception as e:
             print("!!! Error while reading TC: " + str(e))
-            ## FIXME: What if the temperature was freezing?  Then -1 
-            # could be a valid temperature as well as an indicator of 
+            ## FIXME: What if the temperature was freezing?  Then -1
+            # could be a valid temperature as well as an indicator of
             # an error...
-            return "E"   
+            return "E"
+
+class ThermocoupleReader():
+    '''
+    Class to be used for reading batches of thermocouples
+    '''
+    def __init__(self, tc_cs_pins):
+        '''
+        Initialize SPI bus and create thermocouple objects to prepare to read
+
+        :param tc_cs_list: list of cs pins to use to select thermocouples.
+                           The read output will be returned in this order
+        '''
+        self.spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+        self.TCs = []
+        for pin in tc_cs_pins:
+            try:
+                self.TCs.append(Thermocouple(pin, self.spi))
+            except Exception as e:
+                print(e)
+                # Kludge for when the TC doesn't exist
+                self.TCs.append(None)
+
+    def readTCs(self):
+        '''
+        Read Thermocouples in order specified in __init__
+
+        :return: List of thermocouple readings in degC in smae order specified
+                 in the tc_cs_pins argument of __init__
+        '''
+        data = []
+        for TC in self.TCs:
+            try:
+                data.append(TC.readTempC())
+            except Exception:
+                data.append('E[TC]')
+        return data
+
+    def getLastReadings(self):
+        '''
+        Get the last readings from the TCs as a list in order specified
+        when the object was created
+
+        :return: list of previous thermocouple readings in degC
+        '''
+        data = []
+        for TC in self.TCs:
+            data.append(TC.last_reading)
+        return data
 
 def begin():
-    """ returns SPI object to be passed into TC initialization method 
+    """ returns SPI object to be passed into TC initialization method
         when creating thermocouple objects
     """
     spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
@@ -55,16 +103,3 @@ def c_to_f(c):
     """ convert celsius to fahrenheit.
     """
     return (c * 9.0 / 5.0 + 32.0)
-
-
-# Define a function that returns the list of thermocouple measurements 
-def readThermocouples(rocketThermocouples):
-    data = []
-    # for sensor in rocketThermocouples:
-    for i in range(6):
-        try: 
-            data.append(rocketThermocouples[i].readTempC())
-        except Exception as e:
-            print(str(e) + " " + str(i))
-            data.append("E[TC]")
-    return data 
