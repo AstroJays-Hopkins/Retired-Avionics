9ced23b320e1b547d91d71bd2ba6175c94a1474a
Mixih <mxhahn1@gmail.com>
2019-10-17 01:24:32 -0400

    RFHybridController: Formatting Fixes
    Standardized indent to four spaces, one space between if condition and
    opening brace, and linewrap at 80 chars.

--- propulsion/ValveControl/RFHybridController/RFHybridController.ino
+++ propulsion/ValveControl/RFHybridController/RFHybridController.ino
@@ -32,95 +32,98 @@ bool prevVentCommand = true;
 bool newCommand = false;
 
 void setup() {
-  //begin RF communiation
-  LoRa.begin(915E6);
-  LoRa.setTxPower(2);
-  Serial.begin(9600);
-  // Set pullups on switch pins (see schematic)
-  pinMode(bvFwd.pin, INPUT_PULLUP);
-  pinMode(bvRev.pin, INPUT_PULLUP);
-  pinMode(vent.pin, INPUT_PULLUP);
-  // Ignition is different due to LED and should not be pulled up
-  pinMode(ignition.pin, INPUT);
+    //begin RF communiation
+    LoRa.begin(915E6);
+    LoRa.setTxPower(2);
+    Serial.begin(9600);
+    // Set pullups on switch pins (see schematic)
+    pinMode(bvFwd.pin, INPUT_PULLUP);
+    pinMode(bvRev.pin, INPUT_PULLUP);
+    pinMode(vent.pin, INPUT_PULLUP);
+    // Ignition is different due to LED and should not be pulled up
+    pinMode(ignition.pin, INPUT);
 }
 
 void ventCom(){
-  if(getButton(&vent) == LOW && prevVentCommand == true) {
-    CommandV = 1; //indicate venting command was sent
-    prevVentCommand = false;
-    newCommand = true;
-  } else if(getButton(&vent) == HIGH && prevVentCommand == false) {
-    CommandV = 0;
-    prevVentCommand = true;
-    newCommand = true;
-  }
+    if(getButton(&vent) == LOW && prevVentCommand == true) {
+        CommandV = 1; //indicate venting command was sent
+        prevVentCommand = false;
+        newCommand = true;
+    } else if(getButton(&vent) == HIGH && prevVentCommand == false) {
+        CommandV = 0;
+        prevVentCommand = true;
+        newCommand = true;
+    }
 }
 
 void fuelCom() {
-  if (getButton(&fuelOn) == LOW and prevFuelCommand == true){
-    CommandF = 1; //indicate fueling variable was sent
-    prevFuelCommand = false;
-    newCommand = true;
-  } else if(getButton(&fuelOn) == HIGH and prevFuelCommand == false){
-    CommandF = 0; //indicate stop fueling varaible was sent
-    prevFuelCommand = true;
-    newCommand = true;
-  }
+    if (getButton(&fuelOn) == LOW and prevFuelCommand == true){
+        CommandF = 1; //indicate fueling variable was sent
+        prevFuelCommand = false;
+        newCommand = true;
+    } else if(getButton(&fuelOn) == HIGH and prevFuelCommand == false){
+        CommandF = 0; //indicate stop fueling varaible was sent
+        prevFuelCommand = true;
+        newCommand = true;
+    }
 }
 
 void BV() {
-  if (Serial.available()) { // The testing of valve via serial input, which can happen EVEN IF IGNITION LOCK IS LOW
-    trimKey = Serial.read();
-    if (trimKey == 1 or trimKey == 0){
-      BValve = trimKey; //indicate ball valve was driven in the direction determined by the
-      newCommand = true;
+    if (Serial.available()) {
+        // The testing of valve via serial input, which can happen EVEN IF
+        // IGNITION LOCK IS LOW
+        trimKey = Serial.read();
+        if (trimKey == 1 or trimKey == 0) {
+             //indicate ball valve was driven in the direction determined by the
+            BValve = trimKey;
+            newCommand = true;
+        }
     }
-  }
-  if(getButton(&bvFwd) == HIGH && prevBVCommand == false){
-    BValve = 1;
-    prevBVCommand = true;
-    newCommand = true;
-  }
-  else if(getButton(&bvFwd) == LOW && prevBVCommand == true){
-    BValve = 0;
-    prevBVCommand = false;
-    newCommand = true;
-  }
+    if(getButton(&bvFwd) == HIGH && prevBVCommand == false){
+        BValve = 1;
+        prevBVCommand = true;
+        newCommand = true;
+    }
+    else if(getButton(&bvFwd) == LOW && prevBVCommand == true){
+        BValve = 0;
+        prevBVCommand = false;
+        newCommand = true;
+    }
 }
 
 void ignitionCom() {
-  if (getButton(&ignition) == HIGH && prevIgnitionCommand != true){
-    //only execute ignition function if key switch is turned to ON position
-    BValve = 3; //indicate ignition variable was sent
-    prevIgnitionCommand = true;
-    newCommand = true;
-  }
+    if (getButton(&ignition) == HIGH && prevIgnitionCommand != true){
+        //only execute ignition function if key switch is turned to ON position
+        BValve = 3; //indicate ignition variable was sent
+        prevIgnitionCommand = true;
+        newCommand = true;
+    }
 }
 
 void getCom() { //check states, send commands to open/close valves to rocket
-  ventCom();
-  fuelCom();
-  BV();
-  ignitionCom();
+    ventCom();
+    fuelCom();
+    BV();
+    ignitionCom();
 }
 
 void loop() {
-  CommandV = 2; //set states to default holding state variables
-  CommandF = 2;
-  BValve = 2;
-  trimKey = 2;
+    CommandV = 2; //set states to default holding state variables
+    CommandF = 2;
+    BValve = 2;
+    trimKey = 2;
 
-  getCom(); //detect desired states
-  int8_t Command[3] = {CommandV, CommandF, BValve}; //compile data packet to send to solenoid actuator
-  // Serial.println(Command); //DEBUG: display what you sent as the command
-  if(newCommand){
-    for(int i=0; i<3; i++){
-      Serial.print(Command[i]);
-      Serial.println();
+    getCom(); //detect desired states
+    //compile data packet to send to solenoid actuator
+    int8_t Command[3] = {CommandV, CommandF, BValve};
+    // Serial.println(Command); //DEBUG: display what you sent as the command
+    if(newCommand){
+        for(int i=0; i<3; i++){
+            Serial.print(Command[i]);
+            Serial.println();
+        }
+        Serial.println("New Command");
+        Serial.println("------------------------------");
+        newCommand = false;
     }
-    Serial.println("New Command");
-    Serial.println("------------------------------");
-    newCommand = false;
-
-  }
 }
