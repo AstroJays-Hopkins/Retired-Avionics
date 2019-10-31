0a7f3f15861108a10fb53d48880388d8f75e424b
Mixih <mxhahn1@gmail.com>
2019-10-30 22:46:11 -0400

    StepperController: Fix Turn logic
    Missing breaks in switch statement cuased open comands to carry on to
    close commands, thereby actuating a net 0 rotation. this is now fixed
    and the actuation should proceeded as expected. Also, lockouts have been
    added to prevent the valve from actuating in the same direction twice.

--- propulsion/ValveControl/StepperController/StepperController.ino
+++ propulsion/ValveControl/StepperController/StepperController.ino
@@ -2,29 +2,39 @@
 #include<Wire.h>
 
 bool valveState = false;
-int actuate = 0;
-int state = 1;
+volatile uint8_t actuate = 0;
+uint8_t state = 1;
 
 /**
  *  moves the stepper motor  certain number of steps
- *  true is forwards and false is backwards
+ *  true is open (CW) and false is close (CCW)
+ *  DQ542MA DIP Config: {1,3,4,6,7,8} ON, REST OFF
  */
 void move(int deg, bool direction) {
   deg *= 55.55;
   // This is a tenary operator . If the condition is true, the first expression is
   // executed, otherwise the second expression in executed.
-  direction ? digitalWrite(2, LOW) : digitalWrite(2, HIGH);
+  // LOW is CCW and CLOSES the valve
+  // HIGH is CW and OPENS the valve
+  direction ? digitalWrite(2, HIGH) : digitalWrite(2, LOW);
+  Serial.println("moving");
   for(size_t i = 0; i < deg; ++i) {
     digitalWrite(3, HIGH);
     delayMicroseconds(300);
     digitalWrite(3, LOW);
     delayMicroseconds(300);
   }
+  noInterrupts();
   actuate = 0;
+  interrupts();
 }
 
 void handleData() {
   actuate = Wire.read();
+  // clear buffer if the master oversent
+  while(Wire.available()){
+    Wire.read();
+  }
 }
 
 void setup() {
@@ -37,12 +47,18 @@ void setup() {
 }
 
 void loop() {
-  switch(actuate) {
-    case 1:
-      move(90, false);
-      state = 1;
-    case 2:
-      move(90, true);
-      state = 1;
+  if(actuate != 0 && actuate != state) {
+    Serial.println("Actuating!");
+    switch(actuate) {
+        case 1:
+          move(90, false);
+          state = 1;
+          break;
+        case 2:
+          move(90, true);
+          state = 2;
+          break;   
+    }
+    Serial.println("Done!");
   }
 }
